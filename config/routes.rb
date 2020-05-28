2c1b31905b188d9610c598056b1ed01d664c3525
BenMaRe-Dev <b.maguinareyes@gmail.com>
2020-05-27 22:19:04 -0500

    Create the index page for Game and Company

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'companies/index'
+  get 'companies/show'
+resources :games, only: [ :index, :show ]
+resources :companies, only: [ :index, :show ]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
